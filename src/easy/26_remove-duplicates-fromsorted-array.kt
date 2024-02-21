7ae31ef80f086d3172140f065a47ef5df1ca3f67
SebastianFoder <sebastian.foder@gmail.com>
2024-02-21 10:44:48 +0100

    Problem 26 completed

--- src/easy/26_remove-duplicates-fromsorted-array.kt
+++ src/easy/26_remove-duplicates-fromsorted-array.kt
@@ -3,15 +3,18 @@ package easy
 class removeDuplicates {
     fun removeDuplicates(nums: IntArray): Int {
         var corrects = 1;
-        var i = 1;
+        var rightPointer = 1;
+        var leftPointer = 1;
         var prev = nums[0];
-        while(i < nums.size){
-            val current = nums[i];
+        while(rightPointer < nums.size){
+            val current = nums[rightPointer];
             if(current != prev){
                 corrects++;
-                prev = nums[i];
+                prev = current;
+                nums[leftPointer] = prev;
+                leftPointer++;
             }
-            i++;
+            rightPointer++;
         }
         return corrects;
     }
